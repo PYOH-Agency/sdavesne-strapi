b23a2f2ad58594c4e2668debc3c7efe4a4531b4c
pbugeon <paul@pyoh.fr>
2026-01-28 12:08:24 +0100

    Refactor article API to use 'as any' for type assertions in controllers, routes, and services; remove 'publishedAt' field from schema.

--- src/api/article/routes/article.ts
+++ src/api/article/routes/article.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::article.article');
+export default factories.createCoreRouter('api::article.article' as any);
